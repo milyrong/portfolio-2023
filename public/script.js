f8677dfcb1e65e4f6224d72b8ecad9331d1a890b
Mily Rong <mron4486@uni.sydney.edu.au>
2023-06-05 22:06:46 +1000

    Added in the code for the input form to show, added the delete function

--- public/script.js
+++ public/script.js
@@ -10,86 +10,120 @@ function toggleForm() {
       addButton.textContent = "Add New"; // Update button text
     }
   }
-  
-  
-  
-  
-  
-  // Rest of your JavaScript code...
-  
+
 
 //Creating the form input: Code followed from Coding Challenges walk through on Scrimba
-// const form = document.getElementById("taskform");
-// const tasklist = document.getElementById("tasklist");
+const form = document.getElementById("inputform");
+const tasklist = document.getElementById("tasklist");
 
-// var taskList = [];
+//Look for users submitting the form
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTask(
+        form.elements.taskName.value,
+        form.elements.taskRating.value,
+        form.elements.taskDish.value,
+        form.elements.taskReview.value,
+        form.elements.taskComment.value,
+        form.elements.taskDate.value,
+        form.elements.taskFood_img.value,
+        form.elements.taskMeal_type.value,
+        form.elements.taskCuisine.value,
+        form.elements.taskRecommend.value,
+    )
+})
 
-// function addTask(name, rating, dish, review, comment, date, food_img,
-//     meal_type, cuisine, recommend) {
-//     let task = {
-//         name,
-//         rating,
-//         dish,
-//         review,
-//         comment,
-//         date,
-//         food_img,
-//         meal_type,
-//         cuisine,
-//         recommend,
-//         id: Date.now()
+//Display the information that users just inputted 
+function displayTask(task) {
+    let item = document.createElement("li");
+    item.setAttribute("data-id", task.id);
+    item.innerHTML =
+        `<p><strong>${task.name}</strong><br>${task.rating}</p>
+    <span><em>${task.meal_type} hours</em><br>${task.comment}/hr</span>
+   `;
+   tasklist.appendChild(item);
+   form.reset();
+
+    let delButton = document.createElement("button");
+    let delButtonText = document.createTextNode("delete");
+    delButton.appendChild(delButtonText);
+    item.appendChild(delButton);
+
+    delButton.addEventListener("click",function(event){
+        item.remove();
+        taskList.forEach(function(taskArrayElement,taskArrayIndex){
+            if (taskArrayElement.id == item.getAttribute('data-id')){
+                tasklist.splice(taskArrayIndex,1)
+        }
+    }) 
+
+console.log(taskList);
+})
+
+}
+
+//An array to store all the variables 
+var taskList = [];
+
+function addTask(name, rating, dish, review, comment, date, food_img,
+    meal_type, cuisine, recommend) {
+    let task = {
+        name,
+        rating,
+        dish,
+        review,
+        comment,
+        date,
+        food_img,
+        meal_type,
+        cuisine,
+        recommend,
+        id: Date.now()
+    }
+
+    taskList.push(task);
+    displayTask(task);
+}
+
+addTask("Totti",9,"fried","love it","with friends",20/12/2020,"Images/Vecor.png","fancy","chinese",true);
+
+console.log(taskList);
+
+
+
+
+// //defining variable 
+// let checkboxElem = document.createElement("input");
+// checkboxElem.setAttribute("type", "checkbox");
+
+// item.prepend(checkboxElem)
+
+// item.insertBefore(checkboxElem, item.firstChild)
+  
+// checkboxElem.addEventListener("change", (event) => {
+//   // Event listener callback function
+//   let isChecked = event.target.checked;
+  
+//   // for (let i = 0; i < taskList.length; i++) {
+    
+//   // }
+  
+//   taskList.forEach(function(taskArrayElement, taskArrayIndex){
+//     if (taskArrayElement.id == item.getAttribute("data-id")){
+//       if (isChecked) {
+//         item.style.backgroundColor = 'rgb(220, 255, 220)';
+//         taskArrayElement.billable = true;
+//       } else {
+//         item.style.backgroundColor = '#ffffff';
+//         taskArrayElement.billable = false;
+//       }
 //     }
-//     taskList.push(task);
-//     displayTask(task);
-// }
-// form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     addTask(
-//         form.nextElementSibling.taskName.value,
-//         form.nextElementSibling.taskRating.value,
-//         form.nextElementSibling.taskDish.value,
-//         form.nextElementSibling.taskReview.value,
-//         form.nextElementSibling.taskComment.value,
-//         form.nextElementSibling.taskDate.value,
-//         form.nextElementSibling.taskFood_img.value,
-//         form.nextElementSibling.taskMeal_type.value,
-//         form.nextElementSibling.taskCuisine.value,
-//         form.nextElementSibling.taskRecommend.value,
-//     )
+//   })
+  
 // })
 
-// function displayTask(task) {
-//     let item = document.createElement("li");
-//     item.setAttribute("data-id", task.id);
-//     item.innerHTML =
-//         `<p><strong>${task.name}</strong><br>${task.rating}</p>
-//     <span><em>${task.meal_type} hours</em><br>$${task.comment}/hr</span>
-//    `;
-//    tasklist.appendChild(item);
 
-//    form.reset();
-// }
 
-// let delButton = document.createElement("button");
-// let delButtonText = document.createTextNode("🗑️");
-
-// delButton.appendChild(delButtonText);
-// item.appendChild(delButton);
-
-// delButton.addEventListener("click",function(event){
-//     taskList.forEach(function(taskArrayElement,taskArrayIndex)){
-//         if (taskArrayElement.id == item.getAttribute('data-id')){
-//             tasklist.splice(taskArrayIndex,1)
-//         }
-//     }
-// }) 
-
-// console.log(taskList)
-// item.remove();
-
-// let checkboxElem=document.createElement("input");
-// checkboxElem.setAttribute("type","checkbox");
-
-// item.insertBefore(checkboxElem,item.firstChild)
+// Leave the bracket below to close the displayTask function
 
 
